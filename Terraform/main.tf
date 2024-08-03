300cb4d21dcd642f0b56f638d2d1a0dcba86434c
bahrawyX <96246479+bahrawyX@users.noreply.github.com>
2024-08-03 06:15:00 +0300

    typpo

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -269,28 +269,3 @@ resource "aws_iam_role_policy_attachment" "worker_admin_access" {
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
------
-
-output.tf
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane"
-  value       = module.eks.cluster_endpoint
-}
-
-output "cluster_security_group_id" {
-  description = "Security group ids attached to the cluster control plane"
-  value       = module.eks.cluster_security_group_id
-}
-
-output "region" {
-  description = "AWS region"
-  value       = var.region
-}
-
-output "cluster_name" {
-  description = "Kubernetes Cluster Name"
-  value       = module.eks.cluster_name
-}
