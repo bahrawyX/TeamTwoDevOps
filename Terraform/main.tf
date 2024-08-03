fceb86e28509d8d500388063b62c64b2c17e6e2e
bahrawyX <96246479+bahrawyX@users.noreply.github.com>
2024-08-03 04:50:25 +0300

    fixed typpo in main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -154,28 +154,7 @@ resource "aws_lb" "teamtwo_alb" {
   }
 }
 
-# Target Group for routing traffic to EKS
-resource "aws_lb_target_group" "teamtwo_tg" {
-  name     = "TeamTwo-TG"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = module.vpc.vpc_id
-
-  health_check {
-    enabled             = true
-    interval            = 30
-    path                = "/"
-    protocol            = "HTTP"
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 5
-  }
-
-  tags = {
-    Name = "TeamTwo-TG"
-  }
-}
-
+# Target Group for routing traffic to EKS  AKA LOAD BALANCER
 # Listener for the ALB
 resource "aws_lb_target_group" "teamtwo_tg" {
   name     = "TeamTwo-TG"
